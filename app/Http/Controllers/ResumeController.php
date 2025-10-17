dd2afd3b20574be22776d7ad0f6a2807b9be1a09
Phan Cường <phantancuong2002@gmail.com>
2025-10-17 17:27:37 +0700

    fetch resume.json data

--- app/Http/Controllers/ResumeController.php
+++ app/Http/Controllers/ResumeController.php
@@ -3,12 +3,18 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Http\Request;
 
 class ResumeController extends Controller
 {
     //index function
-    public function index(){
+    public function index()
+    {
+
+        $resume = Storage::disk('resume')->get('resume.json');
+
+        dd($resume);
         return view('resume');
     }
 }
