643b7d588ada58f7328e98c2a14d7fdb066f4031
Phan Cường <phantancuong2002@gmail.com>
2025-10-20 11:02:29 +0700

    Add Skill Level enums
    ceate Skill Level enum file and reconfig Skill DataObject file

--- app/DataObjects/Skill.php
+++ app/DataObjects/Skill.php
@@ -4,25 +4,26 @@ declare(strict_types=1);
 
 namespace App\DataObjects;
 
+use App\Enums\SkillLevel;
+
 readonly class Skill
 {
-
-    // construct function
+    /**
+     * @param array<string> $keywords
+     */
     public function __construct(
         public string $name = '',
-        public string $level = '',
-        /** @var string[] */
-        public array $keywords = [],
-    ) {}
+        public ?SkillLevel $level = null,
+        public array  $keywords = []
+    ) {
+    }
 
-
-    // static fromArray() function
     public static function fromArray(array $data): self
     {
         return new self(
-            name: $data['name'] ?? '',
-            level: $data['level'] ?? '',
-            keywords: array_values($data['keywords'] ?? []),
+            name:     $data['name'] ?? '',
+            level:    SkillLevel::fromString($data['level'] ?? ''),
+            keywords: $data['keywords'] ?? []
         );
     }
 }
